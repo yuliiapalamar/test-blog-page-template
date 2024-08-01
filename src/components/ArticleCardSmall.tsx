7081222ebbf103cfb7b4a997ee562a28103765ff
katerynafomina <kf30766@gmail.com>
2024-08-01 05:56:30 +0300

    carousel component for articles

--- src/components/ArticleCardSmall.tsx
+++ src/components/ArticleCardSmall.tsx
@@ -15,7 +15,7 @@ const ArticleCardSmall = ({
   category,
 }: ArticleCardSmallProps) => {
   return (
-    <article className=" overflow-hidden transition-shadow duration-300 ease-in-out hover:shadow-sm focus:shadow-lg">
+    <article className=" overflow-hidden w-full max-w-[370px] lg:max-w-[580px] transition-shadow duration-300 ease-in-out hover:shadow-sm focus:shadow-lg">
       <div className="relative">
         <PrismicNextImage
           field={article.data.image}
@@ -31,19 +31,18 @@ const ArticleCardSmall = ({
         )}
       </div>
       <div className="p-6 pl-0 flex flex-col justify-start items-start">
-        <h3 className="text-xl font-semibold mb-2">
+        <h3 className="text-base lg:text-xl font-semibold text-wrap mb-2">
           {article.data.title || "No Title"}
         </h3>
         <div className="flex flex-row ">
           <p className="text-gray-300 text-xs self-center ">
             {formatDate(article.data.publish_date || "")}
-         </p>
-                  <div className="mx-3 w-px bg-gray-300 my-1"/>
+          </p>
+          <div className="mx-3 w-px bg-gray-300 my-1" />
           {author && (
-            
-              <p className="underline text-xs text-blue-400 self-center">
-                {author.data.name || "No Name"}
-              </p>
+            <p className="underline text-xs text-blue-400 self-center">
+              {author.data.name || "No Name"}
+            </p>
           )}
         </div>
       </div>
--- src/slices/Articles/index.tsx
+++ src/slices/Articles/index.tsx
@@ -2,6 +2,16 @@ import { Content, isFilled } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 import { createClient } from "@/prismicio";
 import ArticleCardSmall from "@/components/ArticleCardSmall";
+import { Bounded } from "@/components/Bounded";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselPrevious,
+  CarouselNext,
+} from "@/components/ui/carousel";
+import CarouseArticlelItem from "@/components/CarouseArticlelItem";
+import Autoplay from "embla-carousel-autoplay";
 
 /**
  * Props for `Articles`.
@@ -26,12 +36,16 @@ const Articles = async ({ slice }: ArticlesProps): Promise<JSX.Element> => {
         const article = await client.getByUID("article", item.article.uid);
 
         if (article && article.data) {
-          const author = isFilled.contentRelationship(article.data.author) && article.data.author.uid
-            ? await client.getByUID("author", article.data.author.uid)
-            : null;
-          const category = isFilled.contentRelationship(article.data.category) && article.data.category.uid
-            ? await client.getByUID("category", article.data.category.uid)
-            : null;
+          const author =
+            isFilled.contentRelationship(article.data.author) &&
+            article.data.author.uid
+              ? await client.getByUID("author", article.data.author.uid)
+              : null;
+          const category =
+            isFilled.contentRelationship(article.data.category) &&
+            article.data.category.uid
+              ? await client.getByUID("category", article.data.category.uid)
+              : null;
 
           return { article, author, category };
         }
@@ -44,35 +58,71 @@ const Articles = async ({ slice }: ArticlesProps): Promise<JSX.Element> => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-     
     >
-      {slice.variation === 'default' &&
-        <div className="grid gap-8 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 py-8 px-60"
+      {slice.variation === "default" && (
+        <Bounded as="section">
+          <div
+            className={`grid gap-8 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 justify-items-center`}
+          >
+            {articles.map(
+              (item: any, index: number) =>
+                item && (
+                  <ArticleCardSmall
+                    key={index}
+                    article={item.article}
+                    author={item.author}
+                    category={item.category}
+                  />
+                )
+            )}
+          </div>
+        </Bounded>
+      )}
+      {slice.variation === "twoColums" && (
+        <Bounded as="section" className="bg-slate-100 items-center">
+          <div
+            className={`grid gap-8 md:grid-cols-2 sm:grid-cols-1 justify-items-center`}
+          >
+            {articles.map(
+              (item: any, index: number) =>
+                item && (
+                  <ArticleCardSmall
+                    key={index}
+                    article={item.article}
+                    author={item.author}
+                    category={item.category}
+                  />
+                )
+            )}
+          </div>
+        </Bounded>
+      )}
+      {slice.variation === "slider" && (
+        // <Bounded as="section">
+        <Carousel
+          article={articles}
+          author={articles.map((a) => a?.author)}
+          category={articles.map((a) => a?.category)}
         >
-          {articles.map((item: any, index: number) => item && (
-            <ArticleCardSmall
-              key={index}
-              article={item.article}
-              author={item.author}
-              category={item.category}
-            />
-          ))}
-        </div>
-      }
-       {slice.variation === 'twoColums' &&
-        <div
-          className="grid gap-8 lg:grid-cols-2 sm:grid-cols-1 py-8 bg-slate-100 px-60"
-        >
-          {articles.map((item: any, index: number) => item && (
-            <ArticleCardSmall
-              key={index}
-              article={item.article}
-              author={item.author}
-              category={item.category}
-            />
-          ))}
-        </div>
-      }
+          <CarouselContent>
+            {articles.map(
+              (item: any, index: number) =>
+                item && (
+                  <CarouselItem key={index}>
+                    <CarouseArticlelItem
+                      article={item.article}
+                      author={item.author}
+                      category={item.category}
+                    />
+                  </CarouselItem>
+                )
+            )}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+        // </Bounded>
+      )}
     </section>
   );
 };
